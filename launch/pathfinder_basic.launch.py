b7904671c0059ab903f23a0865f503d45f1cf324
mayermarci65 <mayermarci65@gmail.com>
2025-11-19 21:07:48 +0100

    launch file 15x15 grid-hez

---
+++ launch/pathfinder_basic.launch.py
@@ -0,0 +1,25 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        # 1. NODE: Az EREDETI pathfinder (A* és DFS logikával)
+        Node(
+            package='var_kqt_feleves',
+            executable='pathfinder_node',   # Figyelj: ez a sima, nem a _100-as!
+            name='pathfinder_node',
+            output='screen',
+            parameters=[
+                {'map_size': 15},          # Visszaállítva a kis méretre
+                {'automatic_mode': True}   # Automata mód (hogy ne kelljen 3. terminál a triggereléshez)
+            ]
+        ),
+
+        # 2. NODE: RViz2
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen'
+        )
+    ])
